4f2950e5256cb1ff395cc81434c02decfa0fcdbf
alazharschool <al.azhar.school.london@gmail.com>
2026-09-07 05:23:59 +0300

    Refactor Tailwind CSS configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -97,14 +97,3 @@ const config: Config = {
 };
 export default config;
 
-module.exports = {
-	content: [
-	  "./app/**/*.{js,ts,jsx,tsx}",
-	  "./components/**/*.{js,ts,jsx,tsx}",
-	],
-	theme: {
-	  extend: {},
-	},
-	plugins: [],
-  }
-  
